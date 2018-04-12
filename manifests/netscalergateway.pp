b7a4c8cb416e3a1f309408ffcddf3ae0ab066c82
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-04-12 18:32:22 +0200

    Typo

--- manifests/netscalergateway.pp
+++ manifests/netscalergateway.pp
@@ -2,9 +2,9 @@
 define xd7storefront::netscalergateway (
   String $netscaler_name,
   String $netscaler_external_url,
-  Enum['UsedForHDXOnly', 'Domain', 'RSA', 'DomainAndRSA', 'SMS', 'GatewayKnows', 'SmartCard', 'None']v$netscaler_authentication_method,
+  Enum['UsedForHDXOnly', 'Domain', 'RSA', 'DomainAndRSA', 'SMS', 'GatewayKnows', 'SmartCard', 'None'] $netscaler_authentication_method,
   String $netscaler_callback_url,
-  String$netscaler_snip,
+  String $netscaler_snip,
   Array[String] $sta_urls,
   Optional[String] $netscaler_smartcardfallbacklogontype = 'None',
 )
